3d94c72127c64870aeb7ae2c3931c9c371ccc0b0
Jack Schefer <jack.schefer9@gmail.com>
2018-08-15 22:35:37 -0400

    use grpc based on env var rather than branch

--- service/railroad_client.py
+++ service/railroad_client.py
@@ -1,22 +1,37 @@
 import grpc
+import os
 
 from . import messages_pb2
 from . import messages_pb2_grpc
 
 PORT = 'localhost:50051'
 
-def get_all_paths():
-    with grpc.insecure_channel(PORT) as channel:
-        stub = messages_pb2_grpc.RailroadBackendStub(channel)
-        response = stub.GetAllPaths(messages_pb2.Empty())
+avoid_grpc = bool(os.getenv('IS_HEROKU', ''))
 
-    return response
+def get_all_paths():
+	if avoid_grpc:
+		from . import railroad_server
+		stub = railroad_server.RailroadServer()
+		return stub.GetAllPaths(messages_pb2.Empty(), None)
+	else:
+		with grpc.insecure_channel(PORT) as channel:
+			stub = messages_pb2_grpc.RailroadBackendStub(channel)
+			response = stub.GetAllPaths(messages_pb2.Empty())
+
+			return response
 
 def perform_search(source, destination):
-    with grpc.insecure_channel(PORT) as channel:
-        stub = messages_pb2_grpc.RailroadBackendStub(channel)
-        response = stub.PerformSearch(messages_pb2.PerformSearchRequest(
-            source=source,
-            destination=destination))
-    
-    return response
+	request = messages_pb2.PerformSearchRequest(
+		source=source,
+		destination=destination)
+
+	if avoid_grpc:
+		from . import railroad_server
+		stub = railroad_server.RailroadServer()
+		return stub.PerformSearch(request)
+	else:
+		with grpc.insecure_channel(PORT) as channel:
+			stub = messages_pb2_grpc.RailroadBackendStub(channel)
+			response = stub.PerformSearch(request)
+
+		return response
